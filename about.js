6709e3b924efb7667b22e8abca50426202ac67b0
LevSarge <levimsargeant@gmail.com>
2021-10-31 19:49:16 -0600

    Finished part 2, most of parts 3 and 4

--- about.js
+++ about.js
@@ -1,14 +1,19 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
+  evt.preventDefault();
+
+  alert("Form has been submitted successfully!");
 }
 
+function rubberDucky(evt) {
+  evt.preventDefault();
 
-let form = document.querySelector('form#contact');
+  alert("I see that you are a man of culture!");
+}
 
-form.addEventListener('submit', handleSubmit);
+let form = document.querySelector("form#contact");
+let ducky = document.querySelector("img#rubber-ducky");
+
+form.addEventListener("submit", handleSubmit);
+ducky.addEventListener("mouseover", rubberDucky);
